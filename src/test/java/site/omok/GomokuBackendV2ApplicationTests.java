c35c5784791d899ea7568e3b77e2b7a876d9704f
kimbeomjoon99 <tnldls99@naver.com>
2024-05-27 10:39:31 +0900

    Initial commit

---
+++ src/test/java/site/omok/GomokuBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.omok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomokuBackendV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
